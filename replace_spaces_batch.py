41625decde6b0a7a8313d8a7cd1c561a20da0454
Jeffrey R. Bell <jeffreybell.ase@gmail.com>
2019-11-29 18:21:48 -0600

    Added os.walk support to batch script

--- replace_spaces_batch.py
+++ replace_spaces_batch.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 
-# Replace spaces with dashes in all of the files and directories in the
-# current path.
+# Find and replace spaces with a new string in all results from a search.
 
 import argparse
 import glob
@@ -10,10 +9,9 @@ import os
 
 import replace_spaces as rs
 
-print('dir(rs) = {}'.format(dir(rs)))
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--search', help='input the search-string to use ' + \
+parser.add_argument('search', help='input the search-string to use ' + \
                     'for finding files and directories to process.')
 
 parser.add_argument('--rstr', default='_', help='set the character ' + \
@@ -27,6 +25,10 @@ parser.add_argument('-i, ', '--interactive', default=False,
                     action='store_true', help='Execute the function ' + \
                     'interactively')
 
+parser.add_argument('-w', '--walk', default=False, action='store_true',
+                    help='Walk through all directories found with the ' + \
+                    'search string')
+
 parser.add_argument('--verbose', '-v', action='count', default=0,
                     help='set the output printing verbosity level')
 
@@ -36,15 +38,63 @@ parser.add_argument('--dry-run', action='store_true', default=False,
 
 args = parser.parse_args()
 
+if args.verbose > 1:
+    print(' ')
+    print('replace_spaces_batch :')
+    print(' ')
+    print('args.search      = {}'.format(args.search))
+    print('args.rstr        = {}'.format(args.rstr))
+    print('args.squash      = {}'.format(args.squash))
+    print('args.interactive = {}'.format(args.interactive))
+    print('args.walk        = {}'.format(args.walk))
+    print('args.verbose     = {}'.format(args.verbose))
+    print('args.dry_run     = {}'.format(args.dry_run))
 
-print(' ')
-print('args.search  = {}'.format(args.search))
-print(' ')
+verbose_nested = max(0, args.verbose-1)
 
-items = glob.glob(args.search)
-for item in items:
-    print('item = {}'.format(item))
+glob_items = glob.glob(args.search)
 
-    rs.replace_spaces(item, rstr=args.rstr, squash=args.squash,
-                      interactive=args.interactive, dry_run=args.dry_run,
-                      verbose=args.verbose)
+if args.verbose > 1:
+    print('glob_items = {}'.format(glob_items))
+
+for glob_item in glob_items:
+
+    if args.verbose > 1:
+        print(' ')
+        print('glob_item = {}'.format(glob_item))
+        print('is file?  = {}'.format(os.path.isfile(glob_item)))
+        print('is dir?   = {}'.format(os.path.isdir(glob_item)))
+
+    if args.walk or True:
+
+        # Walk through any found directories, and process every file.
+        for dir_name, sub_dir_list, file_list in os.walk(glob_item):
+            if args.verbose > 1:
+                print(' ')
+                print('dir_name     = {}'.format(dir_name))
+                print('sub_dir_list = {}'.format(sub_dir_list))
+            if args.verbose > 1:
+                print('file_list    = {}'.format(file_list))
+
+            for file in file_list:
+                file_path = os.path.join(dir_name, file)
+
+                if args.verbose > 0:
+                    print(' ')
+                    print('processing file : {}'.format(file_path))
+
+                rs.replace_spaces(file_path,
+                                  rstr=args.rstr,
+                                  squash=args.squash,
+                                  interactive=args.interactive,
+                                  dry_run=args.dry_run,
+                                  verbose=verbose_nested)
+
+    else:
+
+        rs.replace_spaces(glob_item,
+                          rstr=args.rstr,
+                          squash=args.squash,
+                          interactive=args.interactive,
+                          dry_run=args.dry_run,
+                          verbose=verbose_nested)
